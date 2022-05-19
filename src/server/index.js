59709ac96f575e8072dd10dae3118bf035550473
Martin Hyberg <martin.hyberg@hotmail.com>
2022-05-19 18:00:44 +0300

    Working server

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,16 @@
+
 const express = require('express')
 const app = express()
 const port = 3001
+let games = {}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/createGame', (req, res) => {
+  
+  res.send('Game created')
 })
 
 app.listen(port, () => {
